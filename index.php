dde34d8ad80068db389769bc37b5fe111c671cb0
Amerey <info@amerey.eu>
2023-05-11 19:57:20 +0200

    theme

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="<?php echo Theme::lang() ?>">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <meta name="author" content="Bludit CMS" />
+
+  <?php echo Theme::metaTags('title') ?>
+  <?php echo Theme::metaTags('description') ?>
+  <?php echo Theme::favicon('img/favicon.ico') ?>
+
+  <?php echo Theme::css('css/bundle.min.css'); ?>
+  <?php echo Theme::css('css/main.css') ?>
+  <noscript>
+    <?php echo Theme::css('assets/css/noscript.css') ?>
+  </noscript>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700;800;900&display=swap"
+    rel="stylesheet" />
+
+  <?php Theme::plugins('siteHead') ?>
+</head>
+
+<body>
+  <div class="main-container">
+    <?php Theme::plugins('siteBodyBegin'); ?>
+    <?php include(THEME_DIR_PHP . 'header.php'); ?>
+
+    <?php
+    if ($WHERE_AM_I == 'page') {
+      include(THEME_DIR_PHP . 'page.php');
+    } elseif ($WHERE_AM_I == 'dtsearch') {
+      include(THEME_DIR_PHP . 'search.php');
+    } else {
+      include(THEME_DIR_PHP . 'home.php');
+    }
+    ?>
+
+    <?php include(THEME_DIR_PHP . 'footer.php'); ?>
+  </div>
+
+  <?php echo Theme::javascript('js/bundle.min.js'); ?>
+  <?php Theme::plugins('siteBodyEnd'); ?>
+  </div>
+</body>
+
+</html>
