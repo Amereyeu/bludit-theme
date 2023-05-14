bd02ebfcacaea2357628abf2ba0cb22af96e9b99
Amerey <info@amerey.eu>
2023-05-14 02:44:08 +0200

    theme

--- index.php
+++ index.php
@@ -8,13 +8,14 @@
 
   <?php echo Theme::metaTags('title') ?>
   <?php echo Theme::metaTags('description') ?>
-  <?php echo Theme::favicon('img/favicon.ico') ?>
+  <link rel="apple-touch-icon" sizes="180x180" href="<?php echo Theme::src('img/apple-touch-icon.png') ?>">
+  <link rel="icon" type="image/png" sizes="32x32" href="<?php echo Theme::src('img/favicon-32x32.png') ?>">
+  <link rel="icon" type="image/png" sizes="16x16" href="<?php echo Theme::src('img/favicon-16x16.png') ?>">
+  <link rel="manifest" href="<?php echo Theme::src('img/site.webmanifest') ?>">
 
   <?php echo Theme::css('css/bundle.min.css'); ?>
   <?php echo Theme::css('css/main.css') ?>
-  <noscript>
-    <?php echo Theme::css('assets/css/noscript.css') ?>
-  </noscript>
+
 
   <?php Theme::plugins('siteHead') ?>
 </head>
